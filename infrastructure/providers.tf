b67de683b55e81c2500c6e72db77754b2f11d375
Irem Ertuerk <irem.erturk@thoughtworks.com>
2022-08-23 21:28:33 +0300

    Sets-up the terraform state backend as s3 bucket

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -1,4 +1,12 @@
 terraform {
+    backend "s3" {
+        bucket         = "mlops-zoomcamp-capstone-terraform-state" # local.state-bucket-name
+        key            = "state/terraform.tfstate" 
+        region         = "eu-central-1" # var.region
+        encrypt        = true
+        kms_key_id     = "alias/terraform-bucket-key" #local.state-bucket-kms-alias
+        dynamodb_table = "terraform-state" #dynamodb-state-lock-table
+    }
     required_providers {
         aws = {
         source  = "hashicorp/aws"
