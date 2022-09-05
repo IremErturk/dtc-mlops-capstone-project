97ebe053ebe348cfef3d77309960ea8758253137
Irem Ertuerk <irem.erturk@thoughtworks.com>
2022-09-05 21:45:26 +0300

    Renames the jupyter notebook file

---
+++ workflow_orchestration/constants.py
@@ -0,0 +1,11 @@
+KAGGLE_DATASET_OWNER = "michaelarman"
+KAGGLE_DATASET_NAME = "poemsdataset"
+
+RAW_DATA_LOCAL_PATH = "../data/poems"
+MODELS_LOCAL_PATH = "../data/models"
+
+s3_bucket_name = "mlops-zoomcamp-capstone-artifacts"
+s3_prefect_artifacts = "prefect-artifacts"
+
+RAW_DATA_S3_PATH = f"{s3_bucket_name}/{s3_prefect_artifacts}/raw_data/poems"
+MODELS_S3_PATH = f"{s3_bucket_name}/{s3_prefect_artifacts}/models"
--- workflow_orchestration/flows/datalake_flow.py
+++ workflow_orchestration/flows/datalake_flow.py
@@ -5,21 +5,33 @@ from prefect import flow, task
 from prefect.task_runners import SequentialTaskRunner
 from prefect_shell import shell_run_command
 
+from workflow_orchestration.constants import (
+    KAGGLE_DATASET_NAME,
+    KAGGLE_DATASET_OWNER,
+    RAW_DATA_LOCAL_PATH,
+    RAW_DATA_S3_PATH,
+)
+
 load_dotenv()
 
 
-KAGGLE_DATASET_OWNER = "michaelarman"
-KAGGLE_DATASET_NAME = "poemsdataset"
-OUTPUT_PATH = "../data/poems"
-
-
 @task
-def unzip_dataset():
-    directory_to_extract_to = f"{OUTPUT_PATH}/"
-    path_to_zip_file = f"{OUTPUT_PATH}/{KAGGLE_DATASET_NAME}.zip"
+def unzip_dataset(output_path: str = RAW_DATA_LOCAL_PATH):
+    directory_to_extract_to = f"{output_path}/"
+    path_to_zip_file = f"{output_path}/{KAGGLE_DATASET_NAME}.zip"
     with zipfile.ZipFile(path_to_zip_file, "r") as zip_ref:
         zip_ref.extractall(directory_to_extract_to)
     return True
+
+
+@task
+def upload_folder_to_s3(
+    folder_path: str = RAW_DATA_LOCAL_PATH, bucket_path: str = RAW_DATA_S3_PATH
+):
+    shell_run_command(
+        command=f"aws s3 sync {folder_path} s3://{bucket_path}",
+        return_all=True,
+    )
 
 
 @flow(name="datalake_flow", description="", task_runner=SequentialTaskRunner)
@@ -29,10 +41,11 @@ def datalake_flow():
     and stored in an accessible storage (local filesystem, aws bucket) for the next flow definitions.
     """
     shell_run_command(
-        command=f"kaggle datasets download {KAGGLE_DATASET_OWNER}/{KAGGLE_DATASET_NAME} --path {OUTPUT_PATH}",
+        command=f"kaggle datasets download {KAGGLE_DATASET_OWNER}/{KAGGLE_DATASET_NAME} --path {RAW_DATA_LOCAL_PATH}",
         return_all=True,
     )
-    unzip_dataset()
+    unzip_dataset(output_path=RAW_DATA_LOCAL_PATH)
+    upload_folder_to_s3(folder_path=RAW_DATA_LOCAL_PATH, bucket_path=RAW_DATA_S3_PATH)
 
 
 datalake_flow()
