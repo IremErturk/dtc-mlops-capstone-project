16a037ff16083617fd7c1f54f2e6ede452fd7038
Irem Ertuerk <irem.erturk@thoughtworks.com>
2022-09-07 00:00:57 +0300

    Adds poem-generator model call to the fastapi

--- model_serving/web_service/fastapi_app.py
+++ model_serving/web_service/fastapi_app.py
@@ -1,8 +1,27 @@
+import os
 from typing import Union
+
+from constants import (
+    S3_ARTIFACT_BUCKET_NAME, 
+    S3_PREFECT_PATH,
+    MODELS_PATH,
+    MODELS_LOCAL_PATH, 
+    MODEL_NAME
+)
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastai.text.all import (
+    load_learner,    
+    tensor,
+    torch
+)
+import boto3
+from transformers import GPT2TokenizerFast
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class PoemFeatures(BaseModel):
     baseline: str
@@ -13,6 +32,23 @@ class PoemFeatures(BaseModel):
 
 app = FastAPI()
 
+# initialize some external resources, for faster responses to api
+MODEL_PATH = f"../{MODELS_LOCAL_PATH}/{MODEL_NAME}"
+def init():
+    pretrained_weights = 'gpt2'
+    tokenizer = GPT2TokenizerFast.from_pretrained(pretrained_weights)
+
+     # download model from s3 to local model_path
+    if os.getenv("environment") != "local":
+        s3_client = boto3.client('s3', 
+                            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID") , 
+                            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
+                            )
+        s3_client.download_file(S3_ARTIFACT_BUCKET_NAME, f"{S3_PREFECT_PATH}/{MODELS_PATH}/{MODEL_NAME}", MODEL_PATH)
+    return tokenizer
+
+tokenizer = init()
+
 
 @app.get("/")
 def root():
@@ -21,6 +57,22 @@ def root():
 
 @app.post("/poem/")
 async def create_item(poem_features: PoemFeatures):
-    # do things with poem_features and model stored ..
-    prediction = {"poem": poem_features.baseline}
-    return prediction
+    created_poem = create_poem(baseline=poem_features.baseline)
+    out = {"poem": created_poem}
+    return out
+
+
+def create_poem(baseline:str):
+    baseline_ids = tokenizer.encode(baseline)
+    inp = tensor(baseline_ids)[None]
+
+    # learn = load_learner(learn_path)
+    # preds = learn.model.generate(inp, max_length=60, num_beams=5, no_repeat_ngram_size=2, early_stopping=True)
+    model = torch.load(MODEL_PATH)
+    preds = model.generate(inp, max_length=60, num_beams=5, no_repeat_ngram_size=2, early_stopping=True)
+
+    return tokenizer.decode(preds[0].numpy(), skip_special_tokens=True)
+
+
+# create_poem(baseline="I don't know what I would do")
+# create_poem(baseline="love is ridiculous")
